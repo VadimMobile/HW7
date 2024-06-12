a27358169f4c689385c121a1a7b286af7fd826c2
Admin <qwerty901cx@gmail.com>
2024-06-12 03:25:57 +0300

    Решил без булиня

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        //todo Тут увольняем айтишников
+        //todo Тут увольняем айтишников\
+        int employee = 2;
+        while (employee != 102){
+            System.out.println("Уволен айтишник с id " + employee);
+            employee = employee + 2;
+        }
     }
 }
